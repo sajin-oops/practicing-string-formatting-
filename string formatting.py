3b61507c2c91e47ce882a1f525f0f25a38dda7ec
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-08 22:29:09 +0530

    Add files via upload

---
+++ string formatting.py
@@ -0,0 +1,11 @@
+
+character = "humble"
+
+text = "{guy} is {character}"
+print(text.format(guy = "sajin",character = "humble"))
+
+#O/P - sajin is humble
+
+text = "{name} is {s1} in {company}"
+print(text.format(name="sajin",s1="working",company="google"))
+# - sajin is working in google
